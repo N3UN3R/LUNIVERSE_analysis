0256b2ecf8f2740b154f6c605d1e01c4eab081c1
N3UN3R <77114278+N3UN3R@users.noreply.github.com>
2023-08-10 15:13:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from price_data_analysis import *
+
+
+if __name__ == '__main__':
+
+    #read in data
+    csv_data_2019 = csv_reader("energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2019.csv")
+    data2019 = csv_data_2019.iloc[1:]
+    csv_data_2020 = csv_reader("energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2020.csv")
+    data2020 = csv_data_2020.iloc[1:]
+    csv_data_2021 = csv_reader("energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2021.csv")
+    data2021 = csv_data_2021.iloc[1:]
+    csv_data_2022 = csv_reader("energy-charts_Stromproduktion_und_Börsenstrompreise_in_Deutschland_2022.csv")
+    data2022 = csv_data_2022.iloc[1:]
+
+    #create market prices dataframe
+    market_prices = create_markt_prices_dict(data2019, data2020, data2021, data2022)
+
+    #get monthly data
+    monthly_data = get_hourly_data_for_days(market_prices)
+
+    #dictionaries with minimum prices of each day
+    minimums_2019, minimums_2020, minimums_2021, minimums_2022 = get_minimum_values_dictionaries(monthly_data)
+
+
+
