6b904baf47c964f8dee292e6b2d6c44539c1558e
N3UN3R <77114278+N3UN3R@users.noreply.github.com>
2023-08-15 13:46:54 +0200

    Update saving_calculations.py

--- saving_calculations.py
+++ saving_calculations.py
@@ -1,1 +1,62 @@
+def savings_of_electric_vehicle(price_dict):
+    
+    minimum_values = price_dict.values()
+    df = pd.DataFrame(price_dict.values(), index=price_dict.keys(), columns=['prices'])
+    
+    #power of 5.5 kW over 1 hour assuming 1 hour consumption for each day
+    df['prices'] = df['prices']*5.5
+    total_cost_euro = sum(df['prices'])/100
+    
+    #reference
+    electricity_price = mean_price_2019
+    costs = 2000 * electricity_price/100
+    
+    total_savings = costs -total_cost_euro
+    
+    return df,total_cost_euro, total_savings
 
+df,total_cost_euro, total_savings = savings_of_electric_vehicle(minimums_2019)
+total_savings
+
+def savings_of_water_boiler(price_dict):
+    """ input: minimums2019"""
+    minimum_values = price_dict.values()
+    df = pd.DataFrame(price_dict.values(), index=price_dict.keys(), columns=['prices'])
+    #power of 2 kW over 1 hour assuming 1 hour consumption for each day
+    df['prices'] = df['prices']*2
+    total_cost_euro = sum(df['prices'])/100
+    
+    #reference
+    electricity_price = 30.43
+    costs = 730 * electricity_price/100
+    
+    total_savings = costs -total_cost_euro
+    
+    return df,total_cost_euro, total_savings
+
+
+_,total_cost_2019, total_savings = savings_of_water_boiler(minimums_2019)
+
+total_cost_2019
+total_savings
+
+
+def savings_of_electric_vehicle2(price_dict):
+    
+    minimum_values = price_dict.values()
+    df = pd.DataFrame(price_dict.values(), index=price_dict.keys(), columns=['prices'])
+    
+    #power of 5.5 kW over 1 hour assuming 1 hour consumption for each day
+    df['prices'] = df['prices']*5.5
+    total_cost_euro = sum(df['prices'])/100
+    
+    #reference
+    electricity_price = 30.43
+    costs = 2000 * electricity_price/100
+    
+    total_savings = costs -total_cost_euro
+    
+    return df,total_cost_euro, total_savings
+
+df,total_cost_euro, total_savings = savings_of_electric_vehicle(minimums_2019)
+total_savings
